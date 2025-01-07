f3a4988accf2712079840f5a90b4a1a2df44330d
Yustinus Windy <windykuro@gmail.com>
2025-01-07 08:15:47 +0700

    route home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\DefaultController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DefaultController::class, 'index']);
