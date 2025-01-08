923465e2c2798ba592b5de1a44937582cf31a77c
Yustinus Windy <windykuro@gmail.com>
2025-01-08 09:33:38 +0700

    update

--- app/Http/Controllers/Api/MitraMarketingOrderController.php
+++ app/Http/Controllers/Api/MitraMarketingOrderController.php
@@ -182,6 +182,168 @@ class MitraMarketingOrderController extends Controller
         return response()->json($response);
     }
 
+    public function update(Request $request) {
+        $cek = User::where('api_token',$request->bearerToken())->first();
+        if($cek){
+            $validation = Validator::make($request->all(), [
+                'document_no'       => 'required',
+                'customer_code'     => 'required',
+                'branch_code'       => 'required',
+                'type'              => 'required',
+                'post_date'         => 'required',
+                'valid_date'        => 'required',
+                'type_delivery'     => 'required',
+                'delivery_date'     => 'required',
+                'delivery_schedule' => 'required',
+                'payment_type'      => 'required',
+                'dp_type'           => $request->payment_type == '1' ? 'required' : '',
+                'total'             => 'required',
+                'tax'               => 'required',
+                'grandtotal'        => 'required',
+                'details'           => 'required|array',
+            ], [
+                'document_no.required'       => 'Kode dokumen penghubung tidak boleh kosong.',
+                'customer_code.required'     => 'Kode Customer tidak boleh kosong.',
+                'branch_code.required'       => 'Kode Branch tidak boleh kosong.',
+                'type.required'              => 'Tipe SO tidak boleh kosong. 1 : proyek, 2 : retail, 3 : khusus, 4 : sample.',
+                'post_date.required'         => 'Tanggal post tidak boleh kosong.',
+                'valid_date.required'        => 'Tanggal valid SO tidak boleh kosong.',
+                'type_delivery.required'     => 'Tipe pengiriman tidak boleh kosong. 1 : loco, 2 : franco.',
+                'delivery_date.required'     => 'Tanggal permintaan pengiriman tidak boleh kosong.',
+                'delivery_schedule.required' => 'Status pengiriman tidak boleh kosong. 1 : Segera, 2 : Terjadwal, 3 : Belum Terjadwal.',
+                'payment_type.required'      => 'Tipe pembayaran tidak boleh kosong. 1 : DP, 2 : Kredit.',
+                'dp_type.required'           => 'Tipe DP tidak boleh kosong. 1 : Proporsional, 2 : FIFO.',
+                'total.required'             => 'Total tidak boleh kosong.',
+                'tax.required'               => 'Pajak tidak boleh kosong.',
+                'grandtotal.required'        => 'Grandtotal tidak boleh kosong.',
+                'details.required'           => 'Detail item tidak boleh kosong.',
+                'details.array'              => 'Detail harus dalam bentuk array.',
+            ]);
+
+            if($validation->fails()) {
+                $response = [
+                    'status' => 422,
+                    'error'  => $validation->errors()
+                ];
+            } else {
+
+                DB::beginTransaction();
+                try {
+                    $query = MitraMarketingOrder::where('document_no',$request->document_no)->where('status','1')->where('user_id',$cek->id)->first();
+                    
+                    if($query){
+                        $errorMessage = [];
+                        $customer = User::where('employee_no',$request->customer_code)->where('status','1')->where('type','2')->first();
+                        if(!$customer){
+                            $errorMessage[] = 'Customer tidak ditemukan.';
+                        }
+
+                        if(!in_array($request->type,['1','2','3','4'])){
+                            $errorMessage[] = 'Tipe SO harap diisikan "1" : Proyek / "2" : Retail / "3" : Khusus / "4" : Sample.';
+                        }
+
+                        if(!in_array($request->type_delivery,['1','2'])){
+                            $errorMessage[] = 'Tipe Pengiriman harap diisikan "1" : Loco / "2" : Franco';
+                        }
+
+                        if(!in_array($request->delivery_schedule,['1','2'])){
+                            $errorMessage[] = 'Status pembelian harap diisikan "1" : Segera / "2" : Terjadwal / "3" : Belum Terjadwal.';
+                        }
+
+                        if(!in_array($request->payment_type,['1','2'])){
+                            $errorMessage[] = 'Status pembelian harap diisikan "1" : DP / "2" : Kredit.';
+                        }
+
+                        if($request->payment_type == '1'){
+                            if(!in_array($request->dp_type,['1','2'])){
+                                $errorMessage[] = 'Status pembelian harap diisikan "1" : Proporsional, "2" : FIFO.';
+                            }
+                        }
+
+                        foreach($request->details as $key => $row){
+                            $item = null;
+                            $item = Item::where('code',$row['item_code'])->where('status','1')->first();
+                            if(!$item){
+                                $errorMessage[] = 'Item dengan kode '.$row['item_code'].' tidak ditemukan.';
+                            }
+                        }
+
+                        if(count($errorMessage) == 0){
+                            $query->user_id           = $cek->id;
+                            $query->account_id        = $customer->id;
+                            $query->type              = $request->type;
+                            $query->post_date         = $request->post_date;
+                            $query->valid_date        = $request->valid_date;
+                            $query->document_no       = $request->document_no;
+                            $query->branch_code       = $request->branch_code;
+                            $query->type_delivery     = $request->type_delivery;
+                            $query->delivery_date     = $request->delivery_date;
+                            $query->delivery_schedule = $request->delivery_schedule;
+                            $query->payment_type      = $request->payment_type;
+                            $query->dp_type           = $request->dp_type ?? NULL;
+                            $query->note              = $request->note;
+                            $query->total             = $request->total;
+                            $query->tax               = $request->tax;
+                            $query->grandtotal        = $request->grandtotal;
+                            $query->status            = '1';
+                            $query->save();
+    
+                            $query->mitraMarketingOrderDetail()->delete();
+
+                            foreach($request->details as $key => $row){
+                                $item = Item::where('code',$row['item_code'])->where('status','1')->first(); 
+                                MitraMarketingOrderDetail::create([
+                                    'mitra_marketing_order_id' => $query->id,
+                                    'item_id'                  => $item->id,
+                                    'qty'                      => $row['qty'],
+                                    'price'                    => $row['price'],
+                                    'percent_tax'              => $row['percent_tax'],
+                                    'final_price'              => $row['final_price'],
+                                    'total'                    => $row['total'],
+                                    'tax'                      => $row['tax'],
+                                    'grandtotal'               => $row['grandtotal'],
+                                    'note'                     => $row['note'],
+                                ]);
+                            }
+                            
+                            $response = [
+                                'status'    => 200,
+                                'message'   => 'Data berhasil disimpan',
+                            ];
+                        }else{
+                            $response = [
+                                'status'    => 424,
+                                'error'     => $errorMessage
+                            ];
+                        }
+
+                        DB::commit();
+                    }else{
+                        $response = [
+                            'status'    => 500,
+                            'message'   => 'Data tidak ditemukan.',
+                        ];
+                    }
+                }catch(\Exception $e){
+                    $message = $e->getMessage();
+                    info($message);
+                    DB::rollback();
+                    $response = [
+                        'status'    => 500,
+                        'message'   => $message
+                    ];
+                }
+            }
+        }else{
+            $response = [
+                'status'    => 401,
+                'message'   => 'Token tidak ditemukan'
+            ];
+        }
+
+        return response()->json($response);
+    }
+
     public function getData(Request $request) {
         $cek = User::where('api_token',$request->bearerToken())->first();
         if($cek){
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\HomeController;
 Route::post('login',[HomeController::class, 'login']);
 Route::prefix('mitra_marketing_order')->group(function () {
     Route::post('/create', [MitraMarketingOrderController::class, 'create']);
+    Route::put('/update', [MitraMarketingOrderController::class, 'update']);
     Route::get('/get_data', [MitraMarketingOrderController::class, 'getData']);
     Route::post('/get_data_all', [MitraMarketingOrderController::class, 'getDataAll']);
     Route::post('/destroy', [MitraMarketingOrderController::class, 'destroy']);
