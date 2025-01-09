8ecc098479014c05de66ab60354f246100da9d4d
Andrias Lukman <andribakcang@gmail.com>
2025-01-09 21:13:22 +0700

    stok

---
+++ app/Http/Controllers/Api/TrackingController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class TrackingController extends Controller
+{
+
+    public function getData(Request $request)
+    {
+        $cek = User::where('api_token', $request->bearerToken())->first();
+        $brand_id = $cek->brand_id;
+        if ($cek) {
+
+            $query = DB::select("
+            SELECT a.kode,a.name,a.shading, a.initial as stock FROM (
+        SELECT kode,a.name,a.shading, SUM(qty) AS initial FROM (
+
+        SELECT  d.code AS kode,d.name,k.code AS shading, coalesce(SUM(b.qty*c.conversion),0) AS Qty
+            FROM production_handovers a
+            LEFT JOIN production_handover_details b ON a.id=b.production_handover_id
+            LEFT JOIN production_fg_receive_details c ON c.id=b.production_fg_receive_detail_id and c.deleted_at IS null
+            LEFT JOIN items d ON d.id=b.item_id
+            LEFT JOIN item_shadings k ON k.id=b.item_shading_id
+        WHERE d.brand_id =".$brand_id." and a.void_date IS NULL AND a.deleted_at IS NULL AND d.item_group_id=7 
+        GROUP BY d.code,d.name,k.code
+        UNION ALL
+        SELECT d.code AS kode,d.name,k.code, coalesce(SUM(b.qty),0)*-1 AS RepackOut
+            FROM production_repacks a
+        LEFT JOIN production_repack_details b ON a.id=b.production_repack_id and b.deleted_at is null
+        LEFT JOIN item_units c ON c.id=item_unit_source_id
+        LEFT JOIN items d ON d.id=b.item_source_id
+            LEFT JOIN item_shadings k ON k.id=b.item_shading_id
+            WHERE d.brand_id =".$brand_id." and a.void_date IS NULL AND a.deleted_at IS NULL AND d.item_group_id AND d.item_group_id=7  
+        GROUP BY d.code,d.name,k.code
+        UNION ALL
+        SELECT d.code AS kode,d.name,k.code, coalesce(SUM(b.qty),0) AS RepackIn
+            FROM production_repacks a
+        LEFT JOIN production_repack_details b ON a.id=b.production_repack_id and b.deleted_at is null
+        LEFT JOIN item_units c ON c.id=item_unit_target_id
+        LEFT JOIN items d ON d.id=b.item_target_id
+        LEFT JOIN item_shadings k ON k.id=b.item_shading_id
+            WHERE d.brand_id =".$brand_id." and a.void_date IS NULL AND a.deleted_at IS NULL AND d.item_group_id=7  
+        GROUP BY d.code,d.name,k.code
+        UNION ALL
+        SELECT d.code AS kode,d.name,k.code, coalesce(SUM(b.qty),0) AS GR
+        FROM good_receives a
+        LEFT JOIN good_receive_details b ON a.id=b.good_receive_id and b.deleted_at is null
+        LEFT JOIN items d ON d.id=b.item_id
+        LEFT JOIN item_shadings k ON k.id=b.item_shading_id
+            WHERE d.brand_id =".$brand_id." and a.void_date IS NULL AND a.deleted_at IS NULL AND d.item_group_id=7  
+        GROUP BY d.code,d.name,k.code
+        UNION ALL
+        SELECT d.`code` AS kode,d.name,k.code, coalesce(SUM(b.qty),0)*-1 AS GI
+        FROM good_issues a
+        LEFT JOIN good_issue_details b ON a.id=b.good_issue_id and b.deleted_at is null
+        LEFT JOIN item_stocks c ON c.id=b.item_stock_id
+        LEFT JOIN items d ON d.id=c.item_id
+        LEFT JOIN item_shadings k ON k.id=b.item_shading_id
+            WHERE d.brand_id =".$brand_id." and a.void_date IS NULL AND a.deleted_at IS NULL AND d.item_group_id=7 
+        GROUP BY d.code,d.name,k.code
+        UNION ALL
+        SELECT c.code AS kode,c.name,k.code, coalesce(SUM(b.qty*f.qty_conversion),0)*-1 AS qtySJ
+            FROM marketing_order_delivery_processes a
+            LEFT JOIN marketing_order_delivery_process_details b ON a.id=b.marketing_order_delivery_process_id
+            LEFT JOIN marketing_order_delivery_details e ON e.id=b.marketing_order_delivery_detail_id and e.deleted_at is null
+            LEFT JOIN marketing_order_details f ON f.id=e.marketing_order_detail_id and f.deleted_at is null
+            LEFT JOIN item_stocks l ON l.id=b.item_stock_id
+            LEFT JOIN items c ON c.id=e.item_id
+        LEFT JOIN item_shadings k ON k.id=l.item_shading_id
+            WHERE c.brand_id =".$brand_id." and a.void_date is null AND a.deleted_at is NULL AND c.item_group_id=7  
+        GROUP BY c.code,c.name,k.code)a GROUP BY kode,NAME,shading)a
+
+        ");
+
+
+            if ($query) {
+                $data = [];
+
+                foreach ($query as $row) {
+                    $data[] = [
+                        'code'          => $row->kode,
+                        'name'           => $row->name,
+                        'shading'           => $row->shading,
+                        'qty'         => round($row->stock, 2),
+                        'uom' => 'M2'
+                    ];
+                }
+
+
+                $response = [
+                    'status'    => 200,
+                    'data'      => $data,
+                ];
+            } else {
+                $response = [
+                    'status'    => 401,
+                    'message'   => 'Data tidak ditemukan.'
+                ];
+            }
+        } else {
+            $response = [
+                'status'    => 401,
+                'message'   => 'Token tidak ditemukan'
+            ];
+        }
+
+        return response()->json($response);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MitraMarketingOrderController;
 use App\Http\Controllers\Api\HomeController;
+use App\Http\Controllers\Api\StockController;
+use App\Http\Controllers\Api\TrackingController;
 
 Route::post('login',[HomeController::class, 'login']);
 Route::prefix('mitra_marketing_order')->group(function () {
@@ -12,4 +14,12 @@ Route::prefix('mitra_marketing_order')->group(function () {
     Route::get('/get_data', [MitraMarketingOrderController::class, 'getData']);
     Route::get('/get_data_all', [MitraMarketingOrderController::class, 'getDataAll']);
     Route::post('/destroy', [MitraMarketingOrderController::class, 'destroy']);
+});
+Route::prefix('item_stock')->group(function () {
+    Route::post('/get_data_all', [StockController::class, 'getDataAll']);
+    Route::get('/get_data', [StockController::class, 'getData']);
+});
+
+Route::prefix('tracking')->group(function () {
+    Route::get('/get_data', [TrackingController::class, 'getData']);
 });
