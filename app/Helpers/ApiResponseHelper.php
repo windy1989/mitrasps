974ac7ea387f4db89b7dd07100256257d62f9c0f
Daniel Wongso <danielwongso.work@gmail.com>
2025-01-13 17:07:34 +0700

    Ganti nama class helper dan function

---
+++ app/Helpers/ApiResponseHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Helpers;
+
+class ApiResponseHelper{
+    /*
+        200 OK - Request succeeded.
+        201 Created - Resource successfully created.
+        400 Bad Request - Invalid input or parameters.
+        401 Unauthorized - Authentication failure.
+        404 Not Found - Resource not found.
+        500 Internal Server Error - Server-side issue.
+    */
+
+    /**
+     *  Create consistent API Response
+     *  @param bool $success
+     *  @param int|null $status
+     *  @param string $message
+     *  @param mixed|null $data
+     *  @param mixed $meta
+     *  @return \Illuminate\Http\JsonResponse
+     */
+    
+    public static function apiResponse($success, $status=200, $message, $data=null, $meta=[]){
+        return response()->json([
+            'success' => $success,
+            'status'  => $status,
+            'message' => $message,
+            'data'    => $data, // can contain error ressponse
+            'meta'    => array_merge($meta),
+        ], $status);
+    }
+}
+
+
+
+?>
